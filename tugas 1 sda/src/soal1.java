a43fce61c8bd1dc7fa020d8b2e982bb90d8ba899
dapridandi <91954749+dapridandi@users.noreply.github.com>
2022-02-24 22:41:12 +0700

    Add files via upload

---
+++ tugas 1 sda/src/soal1.java
@@ -0,0 +1,13 @@
+public class soal1 {
+    public static void main(String[] args) {
+        String[] nama= {"A","N","D","I"};
+        //Soal no 1
+        //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+        //isEmty()
+
+        System.out.println(nama[0].isEmpty());     
+        System.out.println(nama[1].isEmpty());     
+        System.out.println(nama[2].isEmpty());     
+        System.out.println(nama[3].isEmpty());     
+     } }
+    
